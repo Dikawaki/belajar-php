59713bb35f9dac982be605f7d605d3cde3234a0a
Ahmad Nur Alamsyah <ahmadalamsyah975@gmail.com>
2022-11-03 19:14:19 +0700

    Mencoba

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Alamsyah";
+$usia = 21;
+echo "Nama saya <strong>$nama</strong>. Saya berusia $usia Tahun. Salam kenal <br>";
+
+$namaAdik = "Nana";
+$umurAdik = 19;
+echo "Nama adik saya <strong>$namaAdik</strong>. Adik saya berusia $umurAdik tahun<br>";
+
+$selisihUmur = $usia - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+?>
